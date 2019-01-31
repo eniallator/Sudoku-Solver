67098e7aad77d3ba7edc248bf9432893a3cd260e
eniallator <eniallatormc@gmail.com>
2019-01-31 16:56:41 +0000

    Added index.html and index.js. WIP solve function in the js

---
+++ index.js
@@ -0,0 +1,34 @@
+function getPossDigits(x, y, puzzle) {
+    const subSquare = { x: 3 * ~~(x / 3), y: 3 * ~~(y / 3) }
+    const digits = new Array(9).map((_, i) => i)
+
+    for (let y = subSquare.y; y < subSquare.y + 3; y++) {
+        for (let x = subSquare.x; x < subSquare.x + 3; x++) {
+            if (!isNaN(puzzle[y][x])) digits[puzzle[y][x]] = null
+        }
+    }
+
+    puzzle.forEach((row, i) =>
+        row.forEach((v, j) => {
+            if ((i === y || j === x) && puzzle[i][j]) digits[puzzle[i][j]] = null
+        })
+    )
+
+    return digits.reduce((acc, v) => {
+        if (!isNaN(v)) acc.push(v)
+    }, [])
+}
+
+function solve(puzzle) {
+    const sortedQueue = []
+
+    for (let y = 0; y < 9; y++) {
+        for (let x = 0; x < 9; x++) {
+            const currItem = { x, y, possDigits: getPossDigits(x, y, puzzle) }
+            const queueIndex = currItem.possDigits.length
+            let i = 0
+            for (; i < sortedQueue.length; i++) if (queueIndex > sortedQueue[i].possDigits.length) break
+            sortedQueue.splice(i, 0, currItem)
+        }
+    }
+}
