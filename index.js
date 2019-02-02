1c9e2ff0ba73499b9e13d5deb013da9add72e51d
eniallator <eniallatormc@gmail.com>
2019-02-02 16:49:46 +0000

    Made the solver make a sorted list of all the possible numbers left to place on the grid

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 function getPossDigits(x, y, puzzle) {
     const subSquare = { x: 3 * ~~(x / 3), y: 3 * ~~(y / 3) }
-    const digits = new Array(9).map((_, i) => i)
+    const digits = new Array(9).fill().map((_, i) => i)
 
     for (let y = subSquare.y; y < subSquare.y + 3; y++) {
         for (let x = subSquare.x; x < subSquare.x + 3; x++) {
-            if (!isNaN(puzzle[y][x])) digits[puzzle[y][x]] = null
+            if (!isNaN(puzzle[y][x])) digits[puzzle[y][x]] = undefined
         }
     }
 
     puzzle.forEach((row, i) =>
         row.forEach((v, j) => {
-            if ((i === y || j === x) && puzzle[i][j]) digits[puzzle[i][j]] = null
+            if ((i === y || j === x) && puzzle[i][j]) digits[puzzle[i][j]] = undefined
         })
     )
 
     return digits.reduce((acc, v) => {
         if (!isNaN(v)) acc.push(v)
+        return acc
     }, [])
 }
 
@@ -24,11 +25,27 @@ function solve(puzzle) {
 
     for (let y = 0; y < 9; y++) {
         for (let x = 0; x < 9; x++) {
-            const currItem = { x, y, possDigits: getPossDigits(x, y, puzzle) }
-            const queueIndex = currItem.possDigits.length
-            let i = 0
-            for (; i < sortedQueue.length; i++) if (queueIndex > sortedQueue[i].possDigits.length) break
-            sortedQueue.splice(i, 0, currItem)
+            if (isNaN(puzzle[y][x])) {
+                const currItem = { x, y, possDigits: getPossDigits(x, y, puzzle) }
+                const queueIndex = currItem.possDigits.length
+                let i = 0
+                for (; i < sortedQueue.length; i++) if (queueIndex < sortedQueue[i].possDigits.length) break
+                sortedQueue.splice(i, 0, currItem)
+            }
         }
     }
+
+    console.log(sortedQueue)
 }
+
+solve([
+    [1, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, 2]
+])
